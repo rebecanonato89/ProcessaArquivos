8e0e4b18cc1903062a3135e4c3d3fb4c433c098e
rebeca.nonato <rebeca.nonato.lemaf@gmail.com>
2020-06-08 08:47:47 -0300

    termino da tarefa

--- src/controllers/CidadesController.js
+++ src/controllers/CidadesController.js
@@ -4,6 +4,8 @@ const contentFilePathCidades = "./src/arquivos/Cidades.json";
 const filePath = "./src/arquivos/";
 let cidades = null;
 let estados = null;
+let estadoMaiorCidade = [];
+let estadoMenorCidade = [];
 
 function loadEstados(){
   const fileBuffer = fs.readFileSync(contentFilePathEstados, "utf8");
@@ -32,6 +34,46 @@ function saveEstadoCidade(estados, cidades) {
     });
 };
 
+function estadosNumCidades(estados){
+  let totalCidades = [];
+  estados.forEach(estado => {
+    cidades = loadCidadesPorEstados(estado.Sigla.toUpperCase());
+    totalCidades.push({'uf': estado.Sigla.toUpperCase(), 'numCidades': cidades.cidadesEstado.length});
+  });
+  return totalCidades;
+};
+
+function retornaMaiorCidade(estados){
+  let bigFor = "";
+  return estados.forEach(estado => {
+    cidades = (loadCidadesPorEstados(estado.Sigla.toUpperCase()));
+    bigFor = "";
+      for (word of cidades.cidadesEstado) {
+        if (word.Nome.trim().length > bigFor.length) {
+          bigFor = word.Nome.trim();
+        }
+      }
+      estadoMaiorCidade.push({"Estado": estado.Nome, "CidadeMaiorNome":bigFor});
+  });
+};
+
+function retornaMenorCidade(estados){
+  let smallFor = "";
+  return estados.forEach(estado => {
+    cidades = (loadCidadesPorEstados(estado.Sigla.toUpperCase()));
+    smallFor = "";
+      for (word of cidades.cidadesEstado) {
+        if (smallFor.length == "") {
+          smallFor = word.Nome.trim();
+        }
+        if (word.Nome.trim().length < smallFor.length) {
+          smallFor = word.Nome.trim();
+        }
+      }
+      estadoMenorCidade.push({"Estado": estado.Nome, "cidadeMenorNome": smallFor});
+  });
+};
+
 module.exports = {
     index(request, response) {
       estados = loadEstados();
@@ -45,17 +87,88 @@ module.exports = {
       let { uf } = request.params;
       let totalCidades;
       cidades = loadCidadesPorEstados(uf.toUpperCase());
-
-
-      // cidades.cidadesEstado.forEach((cidade, index) => {
-      //    totalCidades = index;
-      // });
-
-      // totalCidades = totalCidades + 1;
-
       totalCidades = cidades.cidadesEstado.length;
-
-
       return response.json({ retorno: "O Estado: " + uf + " tem um total de: " + totalCidades + " cidades." });
-    }
+    },
+
+    //Criar um método que imprima no console um array com o UF dos cinco estados que mais possuem cidades, seguidos da quantidade, em ordem decrescente. Utilize o método criado no tópico anterior. Exemplo de impressão: [“UF - 93”, “UF - 82”, “UF - 74”, “UF - 72”, “UF - 65”]
+    calculoMaioresCidades(request, response){
+      estados = loadEstados();
+      let totalCidades = estadosNumCidades(estados);
+
+      totalCidades.sort(function (a, b) {
+        return b.numCidades - a.numCidades;
+      });
+
+      let cincoCidadesMaiores = totalCidades.slice(0, 5);
+
+      response.json({ cincoCidadesMaiores });
+    },
+
+    //Criar um método que imprima no console um array com o UF dos cinco estados que menos possuem cidades, seguidos da quantidade, em ordem decrescente. Utilize o método criado no tópico anterior. Exemplo de impressão: [“UF - 30”, “UF - 27”, “UF - 25”, “UF - 23”, “UF - 21”]
+    calculoMenoresCidades(request, response){
+      estados = loadEstados();
+      let totalCidades = estadosNumCidades(estados);
+
+      totalCidades.sort(function (a, b) {
+        return b.numCidades - a.numCidades;
+      });
+
+      let cincoCidadesMaiores = totalCidades.slice(22);
+
+      response.json({ cincoCidadesMaiores });
+    },
+
+    //Criar um método que imprima no console um array com a cidade de maior nome de cada estado, seguida de seu UF. Em caso de empate, considerar a ordem alfabética para ordená-los e então retornar o primeiro. Por exemplo: [“Nome da Cidade – UF”, “Nome da Cidade – UF”, ...].
+    calculoMaiorNomeCidade(request, response){
+      estados = loadEstados();
+      let maiorCidade = "";
+
+      estadoMaiorCidade = retornaMaiorCidade(estados);
+
+      response.json({ estadoMaiorCidade });
+    },
+
+    //Criar um método que imprima no console um array com a cidade de maior nome de cada estado, seguida de seu UF. Em caso de empate, considerar a ordem alfabética para ordená-los e então retornar o primeiro. Por exemplo: [“Nome da Cidade – UF”, “Nome da Cidade – UF”, ...].
+    calculoMenorNomeCidade(request, response){
+      estados = loadEstados();
+
+      retornaMenorCidade(estados);
+
+      response.json({ estadoMenorCidade });
+    },
+
+    calculoMaiorCidade(request, response){
+      let bigFor = "";
+      estados = loadEstados();
+
+      retornaMaiorCidade(estados);
+
+      for (word of estadoMaiorCidade) {
+        if (word.CidadeMaiorNome.trim().length > bigFor.length) {
+          bigFor = word.CidadeMaiorNome.trim();
+        }
+      }
+
+      response.json({ bigFor });
+    },
+
+    calculoMenorCidade(request, response){
+      let smallFor = "";
+      estados = loadEstados();
+
+      retornaMenorCidade(estados);
+
+      for (word of estadoMenorCidade) {
+        if (smallFor.length == "") {
+          smallFor = word.cidadeMenorNome.trim();
+        }
+        if (word.cidadeMenorNome.trim().length < smallFor.length) {
+          smallFor = word.cidadeMenorNome.trim();
+        }
+      }
+
+      response.json({ smallFor });
+    },
+
 };
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,11 @@ const routes = express.Router();
 
 routes.get('/citys', CidadesController.index);
 routes.post('/citys/:uf', CidadesController.select);
+routes.get('/citysCalculosMaior', CidadesController.calculoMaioresCidades);
+routes.get('/citysCalculosMenor', CidadesController.calculoMenoresCidades);
+routes.get('/citysMaiorNome', CidadesController.calculoMaiorNomeCidade);
+routes.get('/citysMenorNome', CidadesController.calculoMenorNomeCidade);
+routes.get('/citysMaior', CidadesController.calculoMaiorCidade);
+routes.get('/citysMenor', CidadesController.calculoMenorCidade);
 
 module.exports = routes;
